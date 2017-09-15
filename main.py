c71018c0291be07f24dc9df01230a827b0b0baaf
Anton Ozerov <anton.v.ozerov@gmail.com>
2017-09-15 17:10:49 +0300

    version 1.3

--- main.py
+++ main.py
@@ -1,44 +1,95 @@
 import json
 from time import sleep, ctime
-
 import requests
 
-from settings import params, user_id_glob
+
+TOKEN = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
+VERSION = '5.68'
+good_error = frozenset([18, 113])
+# # user_id_glob = 5030613  # id, указанный в задании
+# user_id_glob = 87074577  # id, мой id
+# # user_id_glob = 47936997
+# params = {'access_token': token, 'v': version}
 
 
-def friends_list(params, user_id_glob):
+def check_input_ids():
     """
-    Получаем список id друзей пользователя
+    Получает айди или идентификатор неважно отправляет get запрос на vk
+    и обрабатывает ответ на валидность есть юзер, нет тактого или деактивирован
+    если юзер Валиден то возврашает id и отдает в процесс
     """
-    params['user_id'] = user_id_glob
-    response = requests.get('https://api.vk.com/method/friends.get', params)
+    while True:
+        params = {'access_token': TOKEN,
+                  'v': VERSION,
+                  'user_ids': input('Введите ID для поиска ')
+                  }
+        response = requests.get('https://api.vk.com/method/users.get', params)
+        if response.json().get('error', 'active') == 'active':
+            if response.json()['response'][0].get('deactivated', 'yes') == 'yes':
+                valid_id = response.json()['response'][0]['id']
+            else:
+                print('Пользователь с таким id деактивирован\n')
+                continue
+            break
+        else:
+            print('Пользователя с таким id не существует\n')
+    return valid_id
+
+
+def get_get(method, **kwargs):
+    """
+    Принимает url для get Запроса и параметры, формирует запрос
+    и возврашает ответ, а дальше уже его другие функции разбирают
+    """
+    params = {'access_token': TOKEN, 'v': VERSION}
+    for key, value in kwargs.items():
+        params[key] = value
+    response = requests.get('https://api.vk.com/method/{}'.format(method), params)
+    if (response.json()).get('response'):
+        good_answer = response
+    else:
+        if response.json()['error']['error_code'] in good_error:
+            good_answer = response
+        else:
+            print('Возникла ошибка: {}\nОни говорят: {}'
+                  .format(response.json()['error']['error_code'],
+                          response.json()['error']['error_msg']))
+            exit()
+
+    return good_answer
+
+
+def friends_list(user_id):
+    """
+    Вызываем get_json скармливаем ей url и параметры передаваемые в запросе.
+    Только список друзей возврашает ID пользователей
+    """
+    response = get_get('friends.get', user_id=user_id)
     return (response.json())['response']['items']
 
 
-def user_groups(params, user_id_glob):
+def user_groups(user_id):
     """
-    Получаем группы пользователя
+    Вызываем get_json скармливаем ей url и параметры передаваемые в запросе.
+    Формирует список групп.
     """
-    params['count'] = '1000'
-    params['extended'] = '0'
-    params['user_id'] = user_id_glob
-    response = requests.get('https://api.vk.com/method/groups.get', params)
+    response = get_get('groups.get', count='1000', extended='0', user_id=user_id)
     return response.json()
 
 
-def personal_group(params, friends_list, user_id_glob):
+def personal_group(friends, user_id):
     """
     Получаем группы пользователя,
     получаем группы друзей,
     получаем уникальные группы, которые есть
     у пользователя, но нет у его друзей
     """
-    user_groups_set = set(user_groups(params, user_id_glob)['response']['items'])  # список групп пользователя
+    user_groups_set = set(user_groups(user_id)['response']['items'])  # список групп пользователя
     i = 0
     friends_groups_set = None
-    for user in friends_list:
+    for user in friends:
         i += 1
-        x = user_groups(params, user)  # список групп друга
+        x = user_groups(user)  # список групп друга
         if x.get('error', 'active') == 'active':  # делаем проверку на error
             friends_groups_set = set(x['response']['items'])  # преобразуем в множество
         # находим уникальные группы из 2-х множеств которые принадлежат только 1
@@ -50,14 +101,16 @@ def personal_group(params, friends_list, user_id_glob):
     return user_groups_set
 
 
-def group_info(params, group_ids):
+def group_info(group_ids):
     """
     Получаем информацию о группах,
     готовим список для преобразования в json
     """
-    params['group_ids'] = ','.join(map(str, group_ids))
-    params['fields'] = 'members_count'
-    response = requests.get('https://api.vk.com/method/groups.getById', params)
+    response = get_get(
+        'groups.getById',
+        group_ids=','.join(map(str, group_ids)),
+        fields='members_count'
+    )
     info_groups_list = []
     for group in (response.json())['response']:
         if group.get('deactivated', 'active') == 'active':
@@ -75,22 +128,23 @@ def write_json(data):
 
 
 if __name__ == '__main__':
+    user_id_glob = check_input_ids()
     print('Начало работы скрипта', ctime())
     print('-------------------------')
-    friends_list = friends_list(params, user_id_glob)
-    print('Список друзей сформирован', ctime())
-    print('Друзей', len(friends_list))
+    friends_list = friends_list(user_id_glob)
+    user_group = user_groups(user_id_glob)['response']['items']
+    print('Список друзей и групп пользователя сформирован', ctime())
+    print('Друзей {}\nГрупп {}'.format(len(friends_list), len(user_group)))
     print('-------------------------')
     print('Процесс проверки друзей')
     sleep(1)
-    personal_group = (personal_group(params, friends_list, user_id_glob))
-    print()
+    personal_group = (personal_group(friends_list, user_id_glob))
     print('-------------------------')
     sleep(1)
     print('У данного пользователя {} уникальных групп из {}'
-          .format(len(personal_group), len(user_groups(params, user_id_glob)['response']['items']))
+          .format(len(personal_group), len(user_groups(user_id_glob)['response']['items']))
           )
-    write_json(group_info(params, personal_group))
+    write_json(group_info(personal_group))
     print('-------------------------')
     print('Список групп расположен в файле groups.json')
     print('-------------------------')
--- settings.py
+++ settings.py
@@ -1,8 +1,7 @@
-# token = '2c2e1c13b0705a0eb82c080ec934e42c7e880c0162a2c670572c4dc6630d167b42449f019f0b625293f5d'
-token = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
 version = '5.68'
 nickname = ''
 delay = 2
-user_id_glob = 5030613  # id, указанный в задании
-# user_id_glob = 87074577  # id, мой id
+# user_id_glob = 5030613  # id, указанный в задании
+user_id_glob = 87074577  # id, мой id
+# user_id_glob = 47936997
 params = {'access_token': token, 'v': version}
